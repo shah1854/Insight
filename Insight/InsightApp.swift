399dc11019c10c6ab9ec602d2cc6c4f5f010bf69
shah1854 <shahmihirl07@gmail.com>
2024-06-11 16:01:51 -0500

    Initial Commit

---
+++ Insight/InsightApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsightApp.swift
+//  Insight
+//
+//  Created by Mihir Shah on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct InsightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
